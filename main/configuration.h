611c4ed2db5bf0b9aab6ee3f7d5f6555e13e855c
Max-Plastix <s-github-max@trix.com>
2021-12-19 02:38:42 -0800

    Update Version label

--- main/configuration.h
+++ main/configuration.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // -----------------------------------------------------------------------------
 
 #define APP_NAME                "Helium TTGO"
-#define APP_VERSION             "1.4.4 MaxP"
+#define APP_VERSION             "1.5.1 MaxP"
 
 // -----------------------------------------------------------------------------
 // Configuration
