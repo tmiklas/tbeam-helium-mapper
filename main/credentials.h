59eea70bf8eb45c11b295e54a98a97c1e73dac0c
Max-Plastix <s-github-max@trix.com>
2021-12-24 14:55:21 -0800

    Full reformatting

--- main/credentials.h
+++ main/credentials.h
@@ -2,6 +2,8 @@
 
 #define USE_OTAA
 
-extern const u1_t PROGMEM APPEUI[8];
-extern u1_t DEVEUI[8];
-extern const u1_t PROGMEM APPKEY[16];
+#include <Arduino.h>
+
+extern const uint8_t PROGMEM APPEUI[8];
+extern uint8_t DEVEUI[8];
+extern const uint8_t PROGMEM APPKEY[16];
